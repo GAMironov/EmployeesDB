82afcb168f0fa8169e7bc7e09b1643fdcb403e1a
GAMironov <49306550+GAMironov@users.noreply.github.com>
2019-04-05 13:46:43 +0300

    Add files via upload

---
+++ AuthorizationWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EmployeesDB_Client_6
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "test") && (textBox2.Text == "test"))
+            {
+                this.DialogResult = true;
+                Hide();
+            } else MessageBox.Show("Неверно введен логин или пароль!");
+        }
+    }
+}
